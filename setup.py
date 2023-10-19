ca818d04c92d3b31a89595902b4210bcaf19daff
kkrusere <kuzirusere@gmail.com>
2023-10-19 15:02:34 -0400

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="NHANES-DATA-API",
-    version="0.1.0",
+    version="0.1.1",
     author="Kuzi Rusere",
     author_email="kkrusere@gmail.com",
     description="A tool for programmatic access to NHANES downloadable datasets",
-    long_description="See README.md for details",
+    long_description=long_description,  # Use the content of README.md
+    long_description_content_type="text/markdown",  # Specify the content type
     url="https://github.com/kkrusere/NHANES-Data-API",
     classifiers=[
         "Development Status :: 4 - Beta",
