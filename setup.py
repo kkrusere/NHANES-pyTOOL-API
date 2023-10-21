b441e5bf697dfad4bc728a2a666b669c2195a0cb
kkrusere <kuzirusere@gmail.com>
2023-10-21 12:56:01 -0400

    update

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
 # Read the content of the README.md file
-with open("src/NHANES Data API/README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="NHANES-DATA-API",
-    version="0.1.3",
+    name="nhanes_data_api",
+    version="0.1.0",
     author="Kuzi Rusere",
     author_email="kkrusere@gmail.com",
     description="A tool for programmatic access to NHANES downloadable datasets",
-    long_description=long_description,  # Use the content of README.md
-    long_description_content_type="text/markdown",  # Specify the content type
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/kkrusere/NHANES-Data-API",
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",  # Updated Python version requirement
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
-    packages=find_packages(),
-    python_requires=">=3.8",  # Minimum Python version is 3.8
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.8",
     install_requires=[
         "numpy",
         "pandas",
@@ -28,8 +29,4 @@ setup(
     extras_require={
         'test': ['pytest']
     },
-    # project_urls={
-    #     "Documentation": "https://github.com/kkrusere/NHANES-Data-API/blob/main/docs/index.md",
-    #     "Bug Tracker": "https://github.com/kkrusere/NHANES-Data-API/blob/main/issues/NHANE-DATA-API_issues.md",
-    # },
 )
